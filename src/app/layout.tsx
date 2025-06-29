698a65ad3bff37640d0f9919d7679d4c4ed4b7ea
rafaelomodei <rafael.omodei@outlook.com>
2025-06-28 22:58:48 -0300

    feat: updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,9 @@ import './globals.css';
 
 export const arbutus = Arbutus_Slab({
   subsets: ['latin'],
-  weight: ['400'], // único peso disponível
+  weight: ['400'],
   display: 'swap',
-  variable: '--font-body', // mapeamos p/ CSS var
+  variable: '--font-body',
 });
 
 export const poppins = Poppins({
@@ -28,13 +28,42 @@ celebrar nosso amor ao lado de quem mais amamos. Acompanhe nossa história, conf
 cerimônia, festa e deixe sua mensagem especial para nós.`;
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://mariaerafael.com.br'),
   title: 'Maria e Rafael',
   description: 'Este é o site do nosso casamento Maria e Rafael',
-  metadataBase: new URL('https://mariaerafael.com.br/'),
+
   openGraph: {
     title: 'Maria Eduarda & Rafael Geovani',
     description: shareDescription,
-    images: ['/og-image.png'],
+    type: 'website',
+    url: 'https://mariaerafael.com.br/',
+    images: [
+      {
+        url: 'https://mariaerafael.com.br/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Convite aquarela – Maria Eduarda & Rafael, 27-09-2025',
+      },
+    ],
+  },
+
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Maria Eduarda & Rafael Geovani',
+    description: shareDescription,
+    images: ['https://mariaerafael.com.br/og-image.png'],
+  },
+
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: false,
   },
 };
 
@@ -46,11 +75,13 @@ export default function RootLayout({
   return (
     <html lang='pt-br' suppressHydrationWarning>
       <body
-        className={`${arbutus.className}  
-       ${arbutus.variable}   /* --font-body     */
-          ${poppins.variable}   /* --font-title    */
-          ${arapey.variable}    /* --font-arapey   */
-      antialiased text-primary`}
+        className={`
+          ${arbutus.className}
+          ${arbutus.variable}
+          ${poppins.variable}
+          ${arapey.variable}
+          antialiased text-primary
+        `}
       >
         <main className='text-primary'>{children}</main>
       </body>
