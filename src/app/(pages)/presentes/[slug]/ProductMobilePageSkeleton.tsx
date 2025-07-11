28ea81a0e6262d51649bf18ff364bb91477e8b21
Rafael Omodei <rafael_omodei007@hotmail.com>
2025-07-11 16:02:04 -0300

    fix: align mobile product skeleton with its layout

--- src/app/(pages)/presentes/[slug]/ProductMobilePageSkeleton.tsx
+++ src/app/(pages)/presentes/[slug]/ProductMobilePageSkeleton.tsx
@@ -8,24 +8,11 @@ export default function ProductMobilePageSkeleton() {
     <div className='flex flex-col w-full max-w-6xl gap-4 py-8 px-4'>
       <PageBreadcrumb />
       <Skeleton className='h-8 w-3/4 bg-primary/50' />
+      <Skeleton className='h-4 w-32 bg-primary/50' />
       <Skeleton className='w-full aspect-square bg-primary/50' />
-      <Skeleton className='h-4 w-32 bg-primary/50 mt-2' />
-      <Skeleton className='h-4 w-full bg-primary/50' />
-      <div>
-        <Skeleton className='h-10 w-40 bg-primary/50' />
-        <Skeleton className='h-6 w-24 bg-primary/50 mt-1' />
-      </div>
+      <Skeleton className='h-6 w-1/2 bg-primary/50' />
       <div className='flex flex-col gap-2'>
-        <Skeleton className='h-6 w-40 bg-primary/50' />
-        <div className='flex gap-8'>
-          {Array.from({ length: 4 }).map((_, idx) => (
-            <Skeleton key={idx} className='h-8 w-16 bg-primary/50' />
-          ))}
-        </div>
-      </div>
-      <Skeleton className='h-16 w-full bg-primary/50 mt-10' />
-      <div className='flex flex-col gap-2'>
-        <Skeleton className='h-6 w-40 bg-primary/50' />
+        <Skeleton className='h-6 w-32 bg-primary/50' />
         <Skeleton className='h-4 w-full bg-primary/50' />
         <Skeleton className='h-4 w-3/4 bg-primary/50' />
       </div>
