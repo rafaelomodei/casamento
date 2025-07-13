610c4a8613e88d29e8d31c6713523e5891942ffc
Rafael Omodei <rafael_omodei007@hotmail.com>
2025-07-13 15:11:33 -0300

    Refine mobile product page skeleton

--- src/app/(pages)/presentes/[slug]/ProductMobilePageSkeleton.tsx
+++ src/app/(pages)/presentes/[slug]/ProductMobilePageSkeleton.tsx
@@ -8,9 +8,23 @@ export default function ProductMobilePageSkeleton() {
     <div className='flex flex-col w-full max-w-6xl gap-4 py-8 px-4'>
       <PageBreadcrumb />
       <Skeleton className='h-8 w-3/4 bg-primary/50' />
+      <div className='w-full aspect-square rounded-md bg-primary/50' />
       <Skeleton className='h-4 w-32 bg-primary/50' />
-      <Skeleton className='w-full aspect-square bg-primary/50' />
-      <Skeleton className='h-6 w-1/2 bg-primary/50' />
+      <Skeleton className='h-4 w-full bg-primary/50' />
+      <Skeleton className='h-4 w-3/4 bg-primary/50' />
+      <div>
+        <Skeleton className='h-8 w-40 bg-primary/50' />
+        <Skeleton className='h-6 w-24 bg-primary/50 mt-1' />
+      </div>
+      <div className='flex flex-col gap-2'>
+        <Skeleton className='h-6 w-32 bg-primary/50' />
+        <div className='flex gap-8'>
+          {Array.from({ length: 4 }).map((_, idx) => (
+            <Skeleton key={idx} className='h-8 w-16 bg-primary/50' />
+          ))}
+        </div>
+      </div>
+      <Skeleton className='h-16 w-48 bg-primary/50 mt-10' />
       <div className='flex flex-col gap-2'>
         <Skeleton className='h-6 w-32 bg-primary/50' />
         <Skeleton className='h-4 w-full bg-primary/50' />
